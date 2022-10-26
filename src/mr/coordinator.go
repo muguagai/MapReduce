57609afcb5c7373e839c635d449123393aa5b5c0
fengqingzhong <1183705839@qq.com>
2022-10-26 15:29:12 +0800

    base channel

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,87 @@
 package mr
 
-import "log"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
 import "net"
 import "os"
 import "net/rpc"
 import "net/http"
 
+const (
+	TaskMap    = 0
+	TaskReduce = 1
+	TaskWait   = 2
+	TaskEnd    = 3
+)
 
+type Task struct {
+	FileName  string
+	Id        int
+	StartTime time.Time
+	Status    TaskStatus
+	Wait      chan int
+	JobType   int
+}
+type TaskStatus struct {
+	Timeout bool
+	Done    bool
+}
 type Coordinator struct {
 	// Your definitions here.
+	Files          []string
+	NReduce        int
+	NMap           int
+	Tasks          chan Task
+	HeartbeatCh    chan heartbeatMsg
+	ReportCh       chan reportMsg
+	DoneCh         chan int
+	DoneTask       []Task
+	UnDoneTask     []Task
+	MapDone        bool
+	ReduceDone     bool
+	DoneTaskCount  int
+	ReduceDoneLock sync.Mutex
+}
 
+type heartbeatMsg struct {
+	Response *HeartbeatResponse
+	Ok       chan struct{}
+}
+
+type reportMsg struct {
+	Request *ReportRequest
+	Ok      chan struct{}
+}
+type ReportRequest struct {
+	Task Task
+}
+
+type HeartbeatResponse struct {
+	Task    Task
+	NReduce int
+	NMap    int
+}
+type HeartbeatRequest struct {
+}
+type ReportResponse struct {
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,30 +95,150 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
-	ret := false
+	c.ReduceDoneLock.Lock()
+	ret := c.ReduceDone
+	c.ReduceDoneLock.Unlock()
 
 	// Your code here.
-
-
+	if ret {
+		log.Println("asked whether i am done, replying yes...")
+	} else {
+		log.Println("asked whether i am done, replying no...")
+	}
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
-// nReduce is the number of reduce tasks to use.
-//
+// NReduce is the number of reduce tasks to use.
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
 	// Your code here.
-
-
+	c.Files = files
+	c.NReduce = nReduce
+	c.NMap = len(files)
+	tasks := make(chan Task, len(files)+nReduce)
+	c.UnDoneTask = make([]Task, 0, c.NMap+c.NReduce)
+	// add map task
+	for i := 0; i < len(files); i++ {
+		task := Task{
+			FileName:  files[i],
+			Id:        i,
+			StartTime: time.Now(),
+			JobType:   0,
+			Status: TaskStatus{
+				Done:    false,
+				Timeout: false,
+			},
+			Wait: make(chan int),
+		}
+		tasks <- task
+		c.UnDoneTask = append(c.UnDoneTask, task)
+	}
+	c.DoneTask = make([]Task, 0, c.NMap+c.NReduce)
+	c.Tasks = tasks
+	c.DoneCh = make(chan int, 10)
+	c.ReportCh = make(chan reportMsg)
+	c.HeartbeatCh = make(chan heartbeatMsg)
+	c.MapDone = false
+	c.ReduceDone = false
+	c.DoneTaskCount = 0
 	c.server()
+	//handle heartbeat and report
+	go c.schedule()
 	return &c
+}
+
+func (c *Coordinator) AskTask(request *HeartbeatRequest, reply *HeartbeatResponse) error {
+	reply.Task = <-c.Tasks
+	//No task to give
+	if reply.Task.StartTime.IsZero() {
+		reply.Task = Task{
+			JobType: 2,
+		}
+	}
+	reply.NMap = c.NMap
+	reply.NReduce = c.NReduce
+	msg := heartbeatMsg{reply, make(chan struct{})}
+	reply.NReduce = c.NReduce
+	// give task to worker
+	c.HeartbeatCh <- msg
+	// wait worker finish
+	<-msg.Ok
+	go c.WaitTaskDone(c.UnDoneTask[reply.Task.Id].Wait, reply.Task)
+	reply.Task.StartTime = time.Now()
+	return nil
+}
+
+func (c *Coordinator) Report(request *ReportRequest, response *ReportResponse) error {
+	reportTime := time.Now()
+	if reportTime.Sub(request.Task.StartTime) > 20*time.Second {
+		c.Tasks <- request.Task
+		return errors.New("task timout")
+	}
+	taskId := request.Task.Id
+	c.UnDoneTask[taskId].Wait <- 1
+	msg := reportMsg{request, make(chan struct{})}
+	c.DoneCh <- 1
+	c.ReportCh <- msg
+	<-msg.Ok
+	return nil
+}
+
+func (c *Coordinator) schedule() {
+	for {
+		select {
+		case msg := <-c.HeartbeatCh:
+			msg.Ok <- struct{}{}
+		case msg := <-c.ReportCh:
+			// map task finish,add reduce task
+			if c.DoneTaskCount == c.NMap-1 && !c.MapDone {
+				c.AddReduceTask()
+				c.MapDone = true
+			}
+			Done := <-c.DoneCh
+			if Done == 1 {
+				c.DoneTaskCount++
+				if c.DoneTaskCount == c.NReduce+c.NMap {
+					c.ReduceDoneLock.Lock()
+					c.ReduceDone = true
+					c.ReduceDoneLock.Unlock()
+				}
+			}
+			msg.Ok <- struct{}{}
+		}
+	}
+}
+
+func (c *Coordinator) AddReduceTask() {
+	// add reduce task
+	for i := 0; i < c.NReduce; i++ {
+		task := Task{
+			//FileName:  c.Files[i],
+			Id:        c.NMap + i,
+			StartTime: time.Now(),
+			JobType:   1,
+			Status: TaskStatus{
+				Done:    false,
+				Timeout: false,
+			},
+			Wait: make(chan int),
+		}
+		c.Tasks <- task
+		c.UnDoneTask = append(c.UnDoneTask, task)
+	}
+}
+
+func (c *Coordinator) WaitTaskDone(ch chan int, task Task) {
+	select {
+	case <-time.After(time.Second * 20):
+		fmt.Println("Task 超时")
+		c.Tasks <- task
+	case <-ch:
+		fmt.Println("Task finish")
+	}
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,33 +1,42 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"time"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
@@ -35,45 +44,29 @@ func Worker(mapf func(string, string) []KeyValue,
 
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
-
-}
-
-//
-// example function to show how to make an RPC call to the coordinator.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func CallExample() {
-
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
-	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
-	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
-	} else {
-		fmt.Printf("call failed!\n")
+	for {
+		response := doHeartbeat()
+		log.Printf("Worker: receive coordinator's heartbeat %v \n", response)
+		switch response.Task.JobType {
+		case 0:
+			doMapTask(mapf, response)
+		case 1:
+			doReduceTask(reducef, response)
+		case 2:
+			time.Sleep(1 * time.Second)
+		case 3:
+			return
+		default:
+			panic(fmt.Sprintf("unexpected jobType"))
+		}
 	}
 }
 
-//
-// send an RPC request to the coordinator, wait for the response.
+// send an RPC Request to the coordinator, wait for the Response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
-	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	//c, err := rpc.DialHTTP("tcp", ":1234")
 	sockname := coordinatorSock()
 	c, err := rpc.DialHTTP("unix", sockname)
 	if err != nil {
@@ -88,4 +81,161 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 
 	fmt.Println(err)
 	return false
+}
+
+func doHeartbeat() HeartbeatResponse {
+	// declare an argument structure.
+	request := HeartbeatRequest{}
+
+	// declare a reply structure.
+	reply := HeartbeatResponse{}
+
+	// send the RPC Request, wait for the reply.
+	// the "Coordinator.Example" tells the
+	// receiving server that we'd like to call
+	// the Example() method of struct Coordinator.
+	ok := call("Coordinator.AskTask", &request, &reply)
+	if ok {
+		// reply.Y should be 100.
+		return reply
+	} else {
+		fmt.Printf("call failed!\n")
+	}
+	return HeartbeatResponse{}
+}
+func CallTaskDone(request *ReportRequest) {
+	reply := ExampleReply{}
+	call("Coordinator.Report", &request, &reply)
+}
+func doMapTask(mapf func(string, string) []KeyValue, response HeartbeatResponse) {
+	//intermediate := []KeyValue{}
+	file, err := os.Open(response.Task.FileName)
+	if err != nil {
+		log.Fatalf("cannot open %v", response.Task.FileName)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", response.Task.FileName)
+	}
+	file.Close()
+	intermediate := mapf(response.Task.FileName, string(content))
+	//intermediate = append(intermediate, kva...)
+	// prepare output files and encoders
+	nReduce := response.NReduce
+	//outFiles := make([]*os.File, nReduce)
+	//fileEncs := make([]*json.Encoder, nReduce)
+	/*for outindex := 0; outindex < nReduce; outindex++ {
+		//outname := outprefix + strconv.Itoa(outindex)
+		//outFiles[outindex], _ = os.Create(outname)
+		outFiles[outindex], _ = ioutil.TempFile("mr-tmp", "mr-tmp-*")
+		fileEncs[outindex] = json.NewEncoder(outFiles[outindex])
+	}
+	// distribute keys among mr-fileindex-*
+	for _, kv := range intermediate {
+		outindex := ihash(kv.Key) % nReduce
+		file = outFiles[outindex]
+		enc := fileEncs[outindex]
+		err := enc.Encode(&kv)
+		if err != nil {
+			fmt.Printf("File %v Key %v Value %v Error: %v\n", response.Task.FileName, kv.Key, kv.Value, err)
+			panic("Json encode failed")
+		}
+	}
+	// save as files
+	for outindex, file := range outFiles {
+		outname := fmt.Sprintf("mr-%v-%v", response.Task.Id, outindex)
+		oldpath := filepath.Join(file.Name())
+		//fmt.Printf("temp file oldpath %v\n", oldpath)
+		os.Rename(oldpath, outname)
+		file.Close()
+	}*/
+	kvas := make([][]KeyValue, nReduce)
+	for i := 0; i < nReduce; i++ {
+		kvas[i] = make([]KeyValue, 0)
+	}
+	for _, kv := range intermediate {
+		index := keyReduceIndex(kv.Key, nReduce)
+		kvas[index] = append(kvas[index], kv)
+	}
+	for i := 0; i < nReduce; i++ {
+		tempfile, err := os.CreateTemp(".", "mrtemp")
+		if err != nil {
+			log.Fatal(err)
+		}
+		enc := json.NewEncoder(tempfile)
+		for _, kv := range kvas[i] {
+			err := enc.Encode(&kv)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		outname := fmt.Sprintf("mr-%v-%v", response.Task.Id, i)
+		err = os.Rename(tempfile.Name(), outname)
+		if err != nil {
+			log.Printf("rename tempfile failed for $v\n", outname)
+		}
+	}
+	// acknowledge master
+	CallTaskDone(&ReportRequest{
+		Task: response.Task,
+	})
+}
+
+func doReduceTask(reducef func(string, []string) string, response HeartbeatResponse) {
+	outname := fmt.Sprintf("mr-out-%v", response.Task.Id-response.NMap)
+	innameprefix := "mr-"
+	innamesuffix := "-" + strconv.Itoa(response.Task.Id-response.NMap)
+	// read in all files as a kv array
+	intermediate := []KeyValue{}
+	for index := 0; index < response.NMap; index++ {
+		inname := innameprefix + strconv.Itoa(index) + innamesuffix
+		file, err := os.Open(inname)
+		if err != nil {
+			fmt.Printf("Open intermediate file %v failed: %v\n", inname, err)
+			panic("Open file error")
+		}
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			intermediate = append(intermediate, kv)
+		}
+		file.Close()
+	}
+	sort.Sort(ByKey(intermediate))
+
+	ofile, err := ioutil.TempFile(".", "mrtemp")
+	if err != nil {
+		fmt.Printf("Create output file %v failed: %v\n", outname, err)
+		panic("Create file error")
+	}
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+
+		// this is the correct format for each line of Reduce output.
+		fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
+
+		i = j
+	}
+	os.Rename(filepath.Join(ofile.Name()), outname)
+	ofile.Close()
+	// acknowledge master
+	CallTaskDone(&ReportRequest{
+		Task: response.Task,
+	})
+}
+
+func keyReduceIndex(key string, nReduce int) int {
+	return ihash(key) % nReduce
 }
